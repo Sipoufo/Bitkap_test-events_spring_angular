b0051e2b01fbb37f72627e0262efcc151260a089
sipoufo <sipoufoknj@gmail.com>
2025-07-16 14:43:00 +0100

    feat: init API part

---
+++ event_manager_api/src/main/java/com/bitkap/event_manager_api/EventManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.bitkap.event_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagerApiApplication.class, args);
+	}
+
+}
---
+++ event_manager_api/src/test/java/com/bitkap/event_manager_api/EventManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitkap.event_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
